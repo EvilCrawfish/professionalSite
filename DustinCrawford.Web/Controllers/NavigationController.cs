b402b287de7c9efb985024ebc163a68ba4856f4b
DustinCrawford <dustindjc92@gmail.com>
2016-01-15 10:57:22 -0500

    Add Projects page, link to project page from about page, and send button on contact page

--- DustinCrawford.Web/Controllers/NavigationController.cs
+++ DustinCrawford.Web/Controllers/NavigationController.cs
@@ -24,5 +24,10 @@ namespace DustinCrawford.Web.Controllers
             return View();
         }
 
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
     }
 }
